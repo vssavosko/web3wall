6743c6f31ff1778d7eeaec60d644dee37d0b3dd3
Vladislav Savosko <savosko2009@yandex.ru>
2022-03-26 17:48:27 +0300

    storing and receiving posts

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,15 @@ import { ethers } from 'ethers';
 
 import abi from './utils/Wall.json';
 
+interface Post {
+  user: string;
+  timestamp: number;
+  message: string;
+}
+
 const App = () => {
   const [currentAccount, setCurrentAccount] = useState('');
+  const [allPosts, setAllPosts] = useState([]);
 
   const contractABI = abi.abi;
 
@@ -27,6 +34,8 @@ const App = () => {
         console.log('Found an authorized account:', account);
 
         setCurrentAccount(account);
+
+        getAllPosts();
       } else {
         console.error('No authorized account found');
       }
@@ -50,6 +59,38 @@ const App = () => {
       console.log('Connected', accounts[0]);
 
       setCurrentAccount(accounts[0]);
+
+      getAllPosts();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const getAllPosts = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const wallContract = new ethers.Contract(
+          process.env.REACT_APP_CONTRACT_ADDRESS,
+          contractABI,
+          signer
+        );
+
+        const posts = await wallContract.getAllPosts();
+
+        const postsCleaned = posts.map((post: Post) => ({
+          address: post.user,
+          timestamp: new Date(post.timestamp * 1000),
+          message: post.message,
+        }));
+
+        setAllPosts(postsCleaned);
+      } else {
+        console.error("Ethereum object doesn't exist!");
+      }
     } catch (error) {
       console.error(error);
     }
@@ -72,7 +113,7 @@ const App = () => {
 
         console.log('Retrieved total post count...', count.toNumber());
 
-        const wallTxn = await wallContract.post();
+        const wallTxn = await wallContract.post('A message');
 
         console.log('Mining...', wallTxn.hash);
 
@@ -96,10 +137,27 @@ const App = () => {
   }, []);
 
   return (
-    <div>
-      <button onClick={post}>Create a post</button>
-      {!currentAccount && <button onClick={connectWallet}>Connect Wallet</button>}
-    </div>
+    <>
+      <div>
+        {currentAccount && <button onClick={post}>Create a post</button>}
+        {!currentAccount && <button onClick={connectWallet}>Connect Wallet</button>}
+      </div>
+      {currentAccount && (
+        <div>
+          <h2>Posts</h2>
+          {allPosts.map((post: Post, index: number) => (
+            <div
+              key={index}
+              style={{ backgroundColor: 'OldLace', marginTop: '16px', padding: '8px' }}
+            >
+              <div>Address: {post.user}</div>
+              <div>Time: {post.timestamp.toString()}</div>
+              <div>Message: {post.message}</div>
+            </div>
+          ))}
+        </div>
+      )}
+    </>
   );
 };
 
